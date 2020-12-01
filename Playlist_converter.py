cbfc84c928eea157b07bee1405660162b0ac8957
Jake Klein <jakeklein91@gmail.com>
2020-12-01 23:23:31 +0200

    doesnt add items to playlist, doesnt accept the song URI for some reason.

--- Playlist_converter.py
+++ Playlist_converter.py
@@ -7,4 +7,12 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI))
 
-sp.user_playlist_create(user=USERNAME, name="new new", public=True, collaborative=False, description="")
+playlist_name = "new new"
+playlist_id = 0
+sp.user_playlist_create(user=USERNAME, name=playlist_name, public=True, collaborative=False, description="")
+playlists = sp.user_playlists(user=USERNAME, limit=50)
+for playlist in playlists["items"]:
+    if playlist["name"] == playlist_name:
+        playlist_id = playlist["id"]
+        break
+sp.playlist_add_items(playlist_id=playlist_id, items="spotify:track:2hiuiI3ac0I5kJWtkeGHEL", position=1)  # why?
