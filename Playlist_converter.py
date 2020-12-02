f9c4c2c214614f624674fdfcf23c65d5717dcc4e
Jake Klein <jakeklein91@gmail.com>
2020-12-02 11:59:12 +0200

    creates playlists and adds song successfully

--- Playlist_converter.py
+++ Playlist_converter.py
@@ -2,17 +2,33 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from consts import *
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE,
-                                               client_id=CLIENT_ID,
-                                               client_secret=CLIENT_SECRET,
-                                               redirect_uri=SPOTIPY_REDIRECT_URI))
 
-playlist_name = "new new"
-playlist_id = 0
-sp.user_playlist_create(user=USERNAME, name=playlist_name, public=True, collaborative=False, description="")
-playlists = sp.user_playlists(user=USERNAME, limit=50)
-for playlist in playlists["items"]:
-    if playlist["name"] == playlist_name:
-        playlist_id = playlist["id"]
-        break
-sp.playlist_add_items(playlist_id=playlist_id, items="spotify:track:2hiuiI3ac0I5kJWtkeGHEL", position=1)  # why?
+def create_playlist(sp, playlist_name):
+    sp.user_playlist_create(user=USERNAME, name=playlist_name, public=True, collaborative=False, description="")
+
+
+def get_playlist_id(sp, playlist_name=""):
+    playlists = sp.user_playlists(user=USERNAME, limit=50)
+    for playlist in playlists["items"]:
+        print(playlist["name"])
+        if playlist["name"] == playlist_name:
+            return playlist["id"]
+    return 0
+
+
+def main():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE,
+                                                   client_id=CLIENT_ID,
+                                                   client_secret=CLIENT_SECRET,
+                                                   redirect_uri=SPOTIPY_REDIRECT_URI))
+
+    playlist_name = input("Whats the name of the playlist you want to create?")
+    create_playlist(sp, playlist_name)
+    playlist_id = get_playlist_id(sp, playlist_name)
+    song_id = "spotify:track:1ANYHWUz5NqPu2EBALGK9Z"
+    print(playlist_id)
+    sp.playlist_add_items(playlist_id=playlist_id, items=[song_id])  # why?
+
+
+if __name__ == '__main__':
+    main()
